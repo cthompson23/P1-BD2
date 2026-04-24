4114f51745655157cb3d28c15323e81a882d9d57
Camila Thompson <132382417+cthompson23@users.noreply.github.com>
2026-04-24 14:39:12 -0600

    Refactor database selector to use factory pattern

--- src/config/database_selector.js
+++ src/config/database_selector.js
@@ -1,46 +1,33 @@
-
 const dbType = process.env.DB_TYPE;
 
 if (!dbType) {
   throw new Error("DB_TYPE no definido");
 }
 
-let restaurants_dao;
-let tables_dao;
-let menus_dao;
-let orders_dao;
-let reservations_dao;
-let dishes_dao;
+const allowedDBs = ["mongo", "postgres"];
 
-if (dbType === "mongo") {
-  restaurants_dao = require("../dao/mongo/restaurant_mongo_dao.js");
-  tables_dao = require("../dao/mongo/tables_mongo_dao.js");
-  menus_dao = require("../dao/mongo/menu_mongo_dao.js");
-  orders_dao = require("../dao/mongo/orders_mongo_dao.js");
-  reservations_dao = require("../dao/mongo/reservation_mongo_dao.js");
-  dishes_dao = require("../dao/mongo/dishes_mongo_dao.js");
-
-} else if (dbType === "postgres") {
-  restaurants_dao = require("../dao/postgres/restaurant_postgres_dao.js");
-  tables_dao = require("../dao/postgres/tables_postgres_dao.js");
-  menus_dao = require("../dao/postgres/menu_postgres_dao.js");
-  orders_dao = require("../dao/postgres/orders_postgres_dao.js");
-  reservations_dao = require("../dao/postgres/reservation_postgres_dao.js");
-  dishes_dao = require("../dao/postgres/dishes_postgres_dao.js");
-  
-  restaurants_dao = new restaurants_dao();
-  tables_dao = new tables_dao();
-  menus_dao = new menus_dao();
-  orders_dao = new orders_dao();
-  reservations_dao = new reservations_dao();
-  dishes_dao = new dishes_dao();
+if (!allowedDBs.includes(dbType)) {
+  throw new Error(`DB_TYPE inválido: ${dbType}`);
 }
 
-module.exports = {
-  restaurants_dao,
-  tables_dao,
-  menus_dao,
-  orders_dao,
-  reservations_dao,
-  dishes_dao
-};
+class dao_factory {
+  static create(dbType) {
+    const basePath = `../dao/${dbType}`;
+
+    const load = (name) => {
+      const DAO = require(`${basePath}/${name}_${dbType}_dao.js`);
+      return new DAO();
+    };
+
+    return {
+      restaurants_dao: load("restaurant"),
+      tables_dao: load("tables"),
+      menus_dao: load("menu"),
+      orders_dao: load("orders"),
+      reservations_dao: load("reservation"),
+      dishes_dao: load("dishes")
+    };
+  }
+}
+
+module.exports = dao_factory.create(dbType);
